983a4242b6d839f13806df3abce36b96ee3c55c7
andrew <andrewrfofficial@gmail.com>
2024-01-25 14:16:18 +0100

    1er commit

---
+++ app/src/main/java/com/example/aristidevcursoapp1/superheroapp/SuperHeroAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.aristidevcursoapp1.superheroapp
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SuperHeroAdapter(private val superHero: List<SuperHeroItemResponse> = emptyList()):RecyclerView.Adapter<SuperHeroViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SuperHeroViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/aristidevcursoapp1/superheroapp/SuperHeroViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.aristidevcursoapp1.superheroapp
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SuperHeroViewHolder(view:View):RecyclerView.ViewHolder(view) {
+    fun bind (superHeroItemResponse: SuperHeroItemResponse){
+
+    }
+}
